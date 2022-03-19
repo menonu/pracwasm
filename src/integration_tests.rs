7e9cb917a660166a4146fabbf56ce01530d47441
menonu <menonu3@beorc.info>
2022-03-20 02:48:12 +0900

    add integration tests

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -1,71 +1,107 @@
-// #[cfg(test)]
-// mod tests {
-//     use crate::helpers::CwTemplateContract;
-//     use crate::msg::InstantiateMsg;
-//     use cosmwasm_std::{Addr, Coin, Empty, Uint128};
-//     use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
+#[cfg(test)]
+mod tests {
+    use crate::helpers::CwTemplateContract;
+    use crate::msg::InstantiateMsg;
+    use cosmwasm_std::{Addr, Coin, Empty, Uint128};
+    use cw20_base;
+    use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
 
-//     pub fn contract_template() -> Box<dyn Contract<Empty>> {
-//         let contract = ContractWrapper::new(
-//             crate::contract::execute,
-//             crate::contract::instantiate,
-//             crate::contract::query,
-//         );
-//         Box::new(contract)
-//     }
+    pub fn contract_template() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(
+            crate::contract::execute,
+            crate::contract::instantiate,
+            crate::contract::query,
+        )
+        .with_reply(crate::contract::reply);
+        Box::new(contract)
+    }
 
-//     const USER: &str = "USER";
-//     const ADMIN: &str = "ADMIN";
-//     const NATIVE_DENOM: &str = "denom";
+    pub fn cw20_base_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(
+            cw20_base::contract::execute,
+            cw20_base::contract::instantiate,
+            cw20_base::contract::query,
+        );
+        Box::new(contract)
+    }
 
-//     fn mock_app() -> App {
-//         AppBuilder::new().build(|router, _, storage| {
-//             router
-//                 .bank
-//                 .init_balance(
-//                     storage,
-//                     &Addr::unchecked(USER),
-//                     vec![Coin {
-//                         denom: NATIVE_DENOM.to_string(),
-//                         amount: Uint128::new(1),
-//                     }],
-//                 )
-//                 .unwrap();
-//         })
-//     }
+    const USER: &str = "USER";
+    const ADMIN: &str = "ADMIN";
+    const NATIVE_DENOM: &str = "denom";
 
-//     fn proper_instantiate() -> (App, CwTemplateContract) {
-//         let mut app = mock_app();
-//         let cw_template_id = app.store_code(contract_template());
+    fn mock_app() -> App {
+        AppBuilder::new().build(|router, _, storage| {
+            router
+                .bank
+                .init_balance(
+                    storage,
+                    &Addr::unchecked(USER),
+                    vec![Coin {
+                        denom: NATIVE_DENOM.to_string(),
+                        amount: Uint128::new(1),
+                    }],
+                )
+                .unwrap();
+        })
+    }
 
-//         let msg = InstantiateMsg { count: 1i32 };
-//         let cw_template_contract_addr = app
-//             .instantiate_contract(
-//                 cw_template_id,
-//                 Addr::unchecked(ADMIN),
-//                 &msg,
-//                 &[],
-//                 "test",
-//                 None,
-//             )
-//             .unwrap();
+    fn proper_instantiate() -> (App, CwTemplateContract) {
+        let mut app = mock_app();
+        let cw_template_id = app.store_code(contract_template());
+        let cw20base_id = app.store_code(cw20_base_contract());
 
-//         let cw_template_contract = CwTemplateContract(cw_template_contract_addr);
+        let msg = InstantiateMsg {
+            claimed: Uint128::new(0),
+            token_name: "Piyo token".to_string(),
+            token_symbol: "PIYO".to_string(),
+            cw20_code_id: cw20base_id,
+        };
+        let cw_template_contract_addr = app
+            .instantiate_contract(
+                cw_template_id,
+                Addr::unchecked(ADMIN),
+                &msg,
+                &[],
+                "test",
+                None,
+            )
+            .unwrap();
 
-//         (app, cw_template_contract)
-//     }
+        let cw_template_contract = CwTemplateContract(cw_template_contract_addr);
 
-//     mod count {
-//         use super::*;
-//         use crate::msg::ExecuteMsg;
+        (app, cw_template_contract)
+    }
 
-//         #[test]
-//         fn count() {
-//             let (mut app, cw_template_contract) = proper_instantiate();
+    mod execute {
+        use super::*;
+        use crate::msg::ExecuteMsg;
 
-//             let msg = ExecuteMsg::Increment {};
-//             let cosmos_msg = cw_template_contract.call(msg).unwrap();
-//             app.execute(Addr::unchecked(USER), cosmos_msg).unwrap();
-//         }
-//     }
-// }
+        #[test]
+        fn topup() {
+            let (mut app, cw_template_contract) = proper_instantiate();
+
+            let msg = ExecuteMsg::TopUp {
+                amount: Uint128::new(1_000_000),
+            };
+            let cosmos_msg = cw_template_contract.call(msg).unwrap();
+            app.execute(Addr::unchecked(ADMIN), cosmos_msg).unwrap();
+        }
+
+        #[test]
+        fn topup_then_claim() {
+            let (mut app, cw_template_contract) = proper_instantiate();
+
+            let msg = ExecuteMsg::TopUp {
+                amount: Uint128::new(1_000_000),
+            };
+            let cosmos_msg = cw_template_contract.call(msg).unwrap();
+            app.execute(Addr::unchecked(ADMIN), cosmos_msg).unwrap();
+
+            let msg = ExecuteMsg::Claim {
+                amount: Uint128::new(1_000),
+            };
+            let cosmos_msg = cw_template_contract.call(msg).unwrap();
+            app.execute(Addr::unchecked(USER), cosmos_msg).unwrap();
+        }
+    }
+}
