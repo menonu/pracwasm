7338ac964751fc0a67d3f969c71bf473b83396e0
menonu <menonu3@beorc.info>
2022-03-30 23:55:53 +0900

    Add Cards

---
+++ contracts/game/src/card.rs
@@ -0,0 +1,37 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub enum BJCard {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queeen,
+    King,
+    Ace,
+}
+
+pub type Hand = Vec<BJCard>;
+
+pub const CARDLIST: [BJCard; 13] = [
+    BJCard::Two,
+    BJCard::Three,
+    BJCard::Four,
+    BJCard::Five,
+    BJCard::Six,
+    BJCard::Seven,
+    BJCard::Eight,
+    BJCard::Nine,
+    BJCard::Ten,
+    BJCard::Jack,
+    BJCard::Queeen,
+    BJCard::King,
+    BJCard::Ace,
+];
